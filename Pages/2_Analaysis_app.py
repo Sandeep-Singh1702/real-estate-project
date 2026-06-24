30f0ee4ef270d36aeb080e1b4b1e5ad9cd216ba6
Sandeep-Singh1702 <sandeepssw123@gmail.com>
2026-06-24 15:36:57 +0530

    Add a geo map

--- Pages/2_Analaysis_app.py
+++ Pages/2_Analaysis_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
 st.set_page_config(page_title="Plotting Demo")
 
-st.title("Page 2")
+st.title("Analytics")
+
+new_df = pd.read_csv('data_viz1.csv')
+
+group_df = new_df.groupby('sector')[['price','price_per_sqft','built_up_area','latitude','longitude']].mean()
+
+fig = px.scatter_mapbox(group_df, lat="latitude", lon="longitude",color="price_per_sqft", size="built_up_area",
+                                color_continuous_scale=px.colors.cyclical.IceFire,
+                                zoom=10, mapbox_style="open-street-map",width=1200,
+                                height=700,hover_name=group_df.index)
+
+st.plotly_chart(fig,use_container_width=True)
